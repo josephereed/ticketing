2d092b746b0dc4e06ce222a9a22e3392fb31884b
Joseph Reed <jereed559@gmail.com>
2020-08-31 23:00:53 -0700

    typo fix

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -5,7 +5,7 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://www.ticketeer.xyz',
+      baseURL: 'http://www.ticketeer.xyz/',
       headers: req.headers,
     });
   } else {
