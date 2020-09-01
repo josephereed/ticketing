8da5a072e7f3607938445d7da9ef69741445641b
Joseph Reed <jereed559@gmail.com>
2020-08-31 22:27:41 -0700

    ingress-nginx fix

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -5,7 +5,7 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.kube-system.svc.cluster.local',
+      baseURL: 'http://www.ticketeer.xyz',
       headers: req.headers,
     });
   } else {
