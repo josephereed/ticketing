a6e31f13bd17f376d213e7b94aa83fc7f4eeddb6
Joseph Reed <jereed559@gmail.com>
2020-08-31 22:52:51 -0700

    browser fix

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -5,13 +5,13 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.kube-system.svc.cluster.local',
+      baseURL: 'http://www.ticketeer.xyz',
       headers: req.headers,
     });
   } else {
     // We are on the browser
     return axios.create({
-      baseURL: 'http://www.ticketeer.xyz',
+      baseURL: '/',
     });
   }
 };
